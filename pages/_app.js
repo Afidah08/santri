2e082bd64fe672c01dd4cdbb5fca31b77633976b
Dewi <dewiafidah08@gmail.com>
2022-12-28 15:54:08 +0700

    add supabase

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,21 @@
 import "../styles/globals.css";
 import Navbar from "../components/layout/navbar";
 import Drawer from "../components/layout/drawer";
-
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 function MyApp({ Component, pageProps }) {
+  const [supabase] = useState(() => createBrowserSupabaseClient());
+
   return (
     <>
       <Navbar />
       <Drawer />
-      <Component {...pageProps} />
+      <SessionContextProvider
+        supabaseClient={supabase}
+        initialSession={pageProps.initialSession}
+      >
+        <Component {...pageProps} />
+      </SessionContextProvider>
     </>
   );
 }
