8bf21696e7139114fdd52faf8be2bb4493a89e3f
jagadyudha <jagadyudhaawali@gmail.com>
2023-01-11 19:38:05 +0700

    adjust santri be

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import "../styles/globals.css";
 import React from "react";
 import Layout from "../components/layout/layout";
+import { Toaster } from "react-hot-toast";
+
 function MyApp({ Component, pageProps }) {
   const [noSSR, setNoSSR] = React.useState(false);
 
@@ -10,6 +12,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Toaster />
       {noSSR && (
         <Layout>
           <Component {...pageProps} />
--- pages/api/santri.js
+++ pages/api/santri.js
@@ -33,33 +33,90 @@ export default async function handler(req, res) {
     }
   }
   if (req.method == "POST") {
-    const { data, error } = await supabase
+    const { error: postSantriError, data: postSantriData } = await supabase
       .from("Santri")
-      .select("*, Kriteria(*)")
-      .order("id_santri", { ascending: true });
+      .insert({
+        Nama_lengkap: req.body.Nama_lengkap,
+        Nama_panggilan: req.body.Nama_panggilan,
+        Tanggal_lahir: req.body.Tanggal_lahir,
+        Jenis_kelamin: req.body.Jenis_kelamin,
+        Alamat: req.body.Alamat,
+        Diterima_pada_tanggal: req.body.Diterima_pada_tanggal,
+        Sekolah_formal: req.body.Sekolah_formal,
+        Kelas_sekolah: req.body.Kelas_sekolah,
+        Status: req.body.Status,
+      })
+      .select();
 
-    if (!error) {
-      ahpContext.import({
-        items: getSantriId(data),
-        criteria: ["Kemampuan", "Komitmen", "Riwayat_pendidikan_non_formal"],
-        criteriaItemRank: {
-          Kemampuan: getCriteria("Kemampuan", data),
-          Komitmen: getCriteria("Komitmen", data),
-          Riwayat_pendidikan_non_formal: getCriteria(
-            "Riwayat_pendidikan_non_formal",
-            data
-          ),
-        },
-        criteriaRank: [
-          [1, 0.25, 0.5],
-          [4, 1, 2],
-          [2, 0.5, 1],
-        ],
-      });
+    if (!postSantriError) {
+      const { error: postKriteriaError } = await supabase
+        .from("Kriteria")
+        .insert({
+          id_santri: postSantriData[0].id_santri,
+          Kemampuan: req.body.Kemampuan,
+          Komitmen: req.body.Komitmen,
+          Riwayat_pendidikan_non_formal: req.body.Riwayat_pendidikan_non_formal,
+        });
 
-      const output = ahpContext.run();
+      if (!postKriteriaError) {
+        const { data: santriData, error: santriError } = await supabase
+          .from("Santri")
+          .select("*, Kriteria(*)")
+          .order("id_santri", { ascending: true });
 
-      res.status(200).json(output);
+        if (!santriError) {
+          ahpContext.import({
+            items: getSantriId(santriData),
+            criteria: [
+              "Kemampuan",
+              "Komitmen",
+              "Riwayat_pendidikan_non_formal",
+            ],
+            criteriaItemRank: {
+              Kemampuan: getCriteria("Kemampuan", santriData),
+              Komitmen: getCriteria("Komitmen", santriData),
+              Riwayat_pendidikan_non_formal: getCriteria(
+                "Riwayat_pendidikan_non_formal",
+                santriData
+              ),
+            },
+            criteriaRank: [
+              [1, 0.25, 0.5],
+              [4, 1, 2],
+              [2, 0.5, 1],
+            ],
+          });
+
+          const output = ahpContext.run();
+
+          // mapping class
+          if (output) {
+            const selectedId =
+              output.rankedScoreMap[postSantriData[0].id_santri];
+            if (selectedId >= 0.005 && selectedId < 0.101) {
+              res.status(200).json({
+                score: selectedId,
+                kelas: 1,
+              });
+            } else if (selectedId >= 0.101 && selectedId < 0.115) {
+              res.status(200).json({
+                score: selectedId,
+                kelas: 2,
+              });
+            } else if (selectedId >= 0.115 && selectedId < 0.202) {
+              res.status(200).json({
+                score: selectedId,
+                kelas: 3,
+              });
+            } else if (selectedId >= 0.202) {
+              res.status(200).json({
+                score: selectedId,
+                kelas: 4,
+              });
+            }
+          }
+        }
+      }
     }
   }
 }
